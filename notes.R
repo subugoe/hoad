caccd3dff716b28aa10e8470dfb5ff2c42e23d81
Najko Jahn <najko.jahn@gmail.com>
2017-09-22 17:04:55 +0200

    offsetting, journal und publisher included

--- notes.R
+++ notes.R
@@ -11,28 +11,33 @@ knitr::opts_chunk$set(
 #'
 #' To my knowledge, there's no comprehensive list of hybrid OA journals. However, a list of
 #' hybrid OA journals can be compiled using the Open APC dataset curated by the
-#' [Open APC Initiative](github.com/openapc/openapc-de) initiative. This initiative collects and shares
-#' institutional spending information for open access publication fees, including those spent for publication
-#' in hybrid journals.
+#' [Open APC Initiative](github.com/openapc/openapc-de) initiative. This initiative collects
+#'  and shares institutional spending information for open access publication fees, 
+#'  including those spent for publication in hybrid journals.
 #'
 #' Let's retrieve the most current dataset:
 #'
 library(dplyr)
-# link to dataset
+#' link to dataset
 u <-
   "https://raw.githubusercontent.com/OpenAPC/openapc-de/master/data/apc_de.csv"
 o_apc <- readr::read_csv(u) %>%
   filter(is_hybrid == TRUE)
 #' Some summary statistics:
 #'
+#' We also would like to add data from offsetting aggrements, which is also collected
+#' Open APC initiative, but does not include pricing information.
+#' 
+o_offset <- readr::read_csv("https://raw.githubusercontent.com/OpenAPC/openapc-de/master/data/offsetting/offsetting.csv")
+#' Merge with Open APC dataset
+o_apc <- o_offset %>% 
+  mutate(euro = as.integer(euro)) %>% 
+  bind_rows(o_apc) 
 #' by publishers (top 10)
-library(ggplot2)
 o_apc %>%
   mutate(publisher = forcats::fct_lump(publisher, n = 10)) %>%
-  count(publisher, sort = TRUE) %>%
-  ggplot(aes(reorder(publisher, n), n)) +
-  geom_bar(stat = "identity") +
-  coord_flip()
+  count(publisher) %>%
+  mutate(prop = n / sum(n))
 #'
 #' ## How does it relate to the general hybrid output per journal?
 #'
@@ -40,7 +45,7 @@ o_apc %>%
 #' published per year. The API is accessed via
 #' [rOpenSci's rcrossref client](https://github.com/ropensci/rcrossref).
 #'
-#' Instead of fetching all articles published, we use facet counts.
+#' Instead of fetching all articles published, we use facet counts to keep API usage low
 #'
 #' <https://github.com/CrossRef/rest-api-doc/#facet-counts>
 #'
@@ -57,16 +62,18 @@ jn_facets <- rcrossref::cr_journals(
 #'
 #'
 jn_facets$facets$license
-
 jn_facets$facets$`type-name`
 #'
+#' Relevant facets are also 
+jn_facets$facets$`publisher-name`
+jn_facets$facets$`container-title`
 #' In the first step, get a summary table of articles funded per journal and year
 hybrid_jn <- o_apc %>%
   group_by(journal_full_title, issn, period) %>%
-  summarise(n = n()) %>%
+  summarise(n = n(), euro = sum(euro, na.rm = TRUE)) %>%
   arrange(desc(n))
 hybrid_jn
-#' Now let's fetch facet infos (licenses and work published) for every journal and year
+#' Now let's fetch facet infos (licenses and work published) for every journal 
 #'
 cr_hybrid <-
   purrr::map2(
@@ -90,11 +97,13 @@ cr_hybrid <-
         licence_ref_n = tmp$facets$license$V1,
         year = x,
         issn = y,
-        all_published = tmp$facets$published$V1
+        all_published = tmp$facets$published$V1,
+        publisher = tmp$facets$`publisher-name`$.id,
+        journal = tmp$facets$`container-title`$.id
       )
       }
     })
   )
-cr_hybrid_df <- map_df(cr_hybrid, dplyr::as_data_frame)
+cr_hybrid_df <- purrr::map_df(cr_hybrid, dplyr::as_data_frame)
 #' Backup
 readr::write_csv(cr_hybrid_df, "data/cr_hybrid_df.csv")
