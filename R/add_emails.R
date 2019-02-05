6fe9af102b4b48cf30aeda6c6304115026708f05
Najko Jahn <najko.jahn@gmail.com>
2019-02-05 15:26:47 +0100

    update email info

--- R/add_emails.R
+++ R/add_emails.R
@@ -5,3 +5,4 @@ hybrid <- readr::read_csv("../data/hybrid_publications.csv")
 hybrid %>%
   left_join(emails_df, by = c("doi_oa" = "doi")) %>%
   readr::write_csv("../data/hybrid_publications.csv")
+  
