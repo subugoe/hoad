bbd43c3f66b4ea6099b734b3c775b21027f29f9b
Najko Jahn <najko.jahn@gmail.com>
2019-02-05 15:37:16 +0100

    --amend

--- R/add_emails.R
+++ R/add_emails.R
@@ -5,6 +5,6 @@ hybrid <- readr::read_csv("../data/hybrid_publications.csv") %>%
   select(1:country_name)
 hybrid_emails <- hybrid %>%
   left_join(emails_df, by = c("doi_oa" = "doi")) %>%
-  distinct(doi_oa)
+  distinct(doi_oa, .keep_all = TRUE)
 readr::write_csv(hybrid_emails, "../data/hybrid_publications.csv")
   
