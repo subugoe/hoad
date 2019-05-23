9b42ecb615de6b297e1f4d5c76cbac6a93404595
Najko Jahn <najko.jahn@gmail.com>
2019-05-23 14:50:45 +0200

    add emails

--- R/add_emails.R
+++ R/add_emails.R
@@ -1,6 +1,7 @@
 #' emails add
 library(tidyverse)
-emails_df <- readr::read_csv("../data/emails_normalized.csv")
+emails_df <- readr::read_csv("../data/emails_normalized.csv") %>%
+  distinct(doi, .keep_all = TRUE)
 hybrid <- readr::read_csv("../data/hybrid_publications.csv")
 hybrid %>%
   left_join(emails_df, by = c("doi_oa" = "doi")) %>%
