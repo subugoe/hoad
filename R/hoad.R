3c49fa82bee2bdcac5b75a7caf1eef3d2bbcaee7
Maximilian Held <info@maxheld.de>
2020-05-26 12:16:32 +0200

    add helper function to launch app

--- R/hoad.R
+++ R/hoad.R
@@ -5,3 +5,12 @@
 #' @importFrom rlang .data
 #' @keywords internal
 "_PACKAGE"
+
+#' Start hoad web application
+#' @inheritDotParams shiny::runApp
+#' @inherit shiny::runApp
+#' @family CICD
+#' @export
+runHOAD <- function(...) {
+  rmarkdown::run(file = system.file("app", "dashboard.Rmd", package = "hoad"), ...)
+}
