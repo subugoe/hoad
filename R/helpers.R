d41cc3b5803f56dec5940e70808ed1faab05006b
Maximilian Held <info@maxheld.de>
2020-06-04 12:09:36 +0200

    factor out other oa license closes #204 starts #222

--- R/helpers.R
+++ R/helpers.R
@@ -1,19 +1,19 @@
 #' Find path for files in `/inst/extdata/`
-#' 
+#'
 #' Looks for `inst/` in the working directory, otherwise wraps [fs::path_package()] with appropriate defaults for this package.
-#' 
-#' @details 
+#'
+#' @details
 #' This function **will be deprecated** when all data files are retrieved via APIs.
-#' 
+#'
 #' As a [stop-gap measure](), data files are stored in `inst/extdata/` and compiled into the package.
 #' Depending on the context in which you want to use these files, their paths will differ.
 #' When running from the source root, they can be found at `inst/extdata/`.
 #' When the package is compiled (as in vignette use), they can be found at `extadata/`.
-#' 
+#'
 #' This function automatically picks the correct path or throws an error if the file cannot be found.
 #'
 #' @param x `[character(1)]` giving the filename or path to the file from `inst/extdata/`
-#' 
+#'
 #' @return `[character(1)]` giving the absolute path to `x`, as returned by [fs::path_abs()].
 #'
 #' @keywords internal
@@ -27,4 +27,17 @@ path_extdat <- function(x) {
   }
   checkmate::assert_file_exists(x = res)
   res
+}
+
+#' Find all integers inside the range of a vector
+#'
+#' Helpful for finding all years.
+#' [unique()] is't quite the same, because it would leave out empty years, but for plots and the like there can never be empty years.
+#'
+#' @param x a numeric vector that can be transformed to integers (integerish)
+#' @return an integer vector from min to max
+find_all_ints <- function(x) {
+  checkmate::assert_integerish(x)
+  x <- as.integer(x)
+  min(x):max(x)
 }
